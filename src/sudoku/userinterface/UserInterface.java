c6e4d9bf5bc575a643a015eda5b4d3557ea69956
Melody <leclecticat@gmail.com>
2020-08-07 15:03:38 -0700

    "setting up User Interface logic structure and creating function to draw grid lines via rectangle"

---
+++ src/sudoku/userinterface/UserInterface.java
@@ -0,0 +1,169 @@
+package sudoku.userinterface;
+
+import com.sun.javafx.geom.Rectangle;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import sudoku.problemdomain.Coordinates;
+import sudoku.problemdomain.SudokuGame;
+
+import java.util.HashMap;
+
+public class UserInterface implements IUserInterfaceContract.View,
+        EventHandler<KeyEvent> {
+
+    private final Stage stage;
+    private final Group root;
+
+    private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
+    
+    private IUserInterfaceContract.EventListener listener;
+    
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 732;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_Y = 576;
+
+    // CSS
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(i:0, i1:150, i2: 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(i:224, i1:242, i2:241);
+    private static final String SUDOKU = "Sudoku";
+
+    public UserInterface(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
+    }
+
+    //call helper methods to set up the sudoku board and underlying logic
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
+    }
+
+    private void drawBackground(Group root) {
+
+    }
+
+    private void drawTitle(Group root) {
+
+    }
+
+    private void drawSudokuBoard(Group root) {
+
+    }
+
+    private void drawTextFields(Group root) {
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+        final int xAndYDelta = 64;
+
+        //n^2 runtime complexity
+        for (int xIndex =0; xIndex < 9; xIndex++) {
+            for(int yIndex = 0; yIndex < 9; yIndex++) {
+                int x = xOrigin + xIndex * xAndYDelta;
+                int y = yOrigin + yIndex * xAndYDelta;
+
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+
+                styleSudokuTile(tile, x, y);
+            }
+        }
+
+    }
+
+    private void styleSudokuTile(SudokuTextField tile, double x, double y) {
+        Font numberFont = new Font (v:32);
+
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+    }
+
+    private void drawGridLines(Group root) {
+        int xAndY = 114;
+        int index = 0;
+        while (index < 8) {
+            int thickness;
+            if (index == 2 || index == 5 ) {
+                thickness = 3;
+            }
+            else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+            );
+            
+             Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    xAndY + 64 * index,
+                    thickness,
+                     BOARD_X_AND_Y
+            );
+
+           root.getChildren().addAll(
+                    verticalLine,
+                    horizontalLine
+           );
+
+           index++;
+        }
+    }
+
+    private Rectangle getLine(double x, double y, double height, double width){
+        Rectangle line = new Rectangle();
+
+        line.setX(x);
+        line.setY(y);
+        line.setHeight(height);
+        line.setWidth(width);
+
+        line.setFill(Color.BLACK);
+        return line;
+
+    }
+
+    @Override
+    public void setListener(IUserInterfaceContract.EventListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void updateSquare(int x, int y, int input) {
+
+    }
+
+    @Override
+    public void updateBoard(SudokuGame game) {
+
+    }
+
+    @Override
+    public void showDialog(String Message) {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+    
+    @Override
+    public void handle(KeyEvent keyEvent) {
+    }
+}
